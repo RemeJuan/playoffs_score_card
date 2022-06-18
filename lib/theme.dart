13554300e084a5a485d2a06cb3cb2958fe6186ac
Reme Le Hane <mail@remej.dev>
2022-06-18 21:13:27 +0200

    style(score_card): updating styles

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class AppTheme {
   static const paddingDefault = 8.0;
+  static const bottomBarHeight = 56.0;
 
   static final ThemeData primary = ThemeData(
     fontFamily: "Trade Gothic",
     useMaterial3: true,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: HexColor("0c0033"),
+      selectedItemColor: HexColor("fb053e"),
+      unselectedItemColor: HexColor("ffffff"),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: HexColor("fb053e"),
+        onPrimary: HexColor("ffffff"),
+      ),
+    ),
   );
 }
