138ad896ec85db67350a73a988193727e0ea660e
Reme Le Hane <mail@remej.dev>
2022-06-19 11:43:24 +0200

    feat(app): getting data into history view

--- lib/views/history/history.view.dart
+++ lib/views/history/history.view.dart
@@ -4,33 +4,44 @@ import 'package:playoffs_score_tracker/views/history/provider/history.provider.d
 import 'package:playoffs_score_tracker/views/history/widgets/history_chart.dart';
 import 'package:playoffs_score_tracker/views/history/widgets/history_entry.dart';
 import 'package:provider/provider.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class HistoryView extends StatelessWidget {
   const HistoryView({Key? key}) : super(key: key);
 
   @override
   Widget build(context) {
-    return ChangeNotifierProvider<HistoryProvider>.value(
-      value: sl<HistoryProvider>()..init(),
-      child: Consumer<HistoryProvider>(
-        builder: (context, provider, _) {
-          return Column(
-            children: [
-              const HistoryChart(),
-              Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: provider.scores.length,
-                  itemBuilder: (context, index) {
-                    final score = provider.scores[index];
+    final provider = sl<HistoryProvider>()..init();
 
-                    return HistoryEntry(score: score);
-                  },
+    return VisibilityDetector(
+      key: UniqueKey(),
+      onVisibilityChanged: (visibility) {
+        if (visibility.visibleFraction == 1.0) {
+          provider.init();
+        }
+      },
+      child: ChangeNotifierProvider<HistoryProvider>.value(
+        value: provider,
+        child: Consumer<HistoryProvider>(
+          builder: (context, provider, _) {
+            return Column(
+              children: [
+                const HistoryChart(),
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: provider.scores.length,
+                    itemBuilder: (context, index) {
+                      final score = provider.scores[index];
+
+                      return HistoryEntry(score: score);
+                    },
+                  ),
                 ),
-              ),
-            ],
-          );
-        },
+              ],
+            );
+          },
+        ),
       ),
     );
   }
