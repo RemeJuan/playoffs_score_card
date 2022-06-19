488792e6ef5882c27e361d000ce8cb01419165d0
Reme Le Hane <mail@remej.dev>
2022-06-19 19:19:56 +0200

    feat(app): setting up firebase

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 
+import 'firebase_options.dart';
 import 'locator.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
@@ -13,12 +16,19 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
 
   await sl.isReady<Isar>();
 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAppCheck.instance.activate(
+    webRecaptchaSiteKey: 'playoffs-score-card-re',
+  );
+
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
   await runZonedGuarded(
-        () async => runApp(await builder()),
-        (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+    () async => runApp(await builder()),
+    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
   );
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_app_check
+import firebase_core
+import firebase_database
 import isar_flutter_libs
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
