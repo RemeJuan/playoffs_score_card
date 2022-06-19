18b253be2edaf8c6a4d739dea344ff33cd54928b
Reme Le Hane <mail@remej.dev>
2022-06-19 10:41:51 +0200

    none(scores): register router provider

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,11 +1,19 @@
 import 'package:get_it/get_it.dart';
+import 'package:playoffs_score_tracker/router/router_provider.dart';
+import 'package:playoffs_score_tracker/schemas/score_card.schema.dart';
 import 'package:playoffs_score_tracker/views/score_card/provider/score_card_provider.dart';
-import 'package:uuid/uuid.dart';
+import 'package:realm/realm.dart';
+import 'package:uuid/uuid.dart' as u;
 
 final sl = GetIt.instance;
 
 void initService() {
+  sl.registerLazySingleton<RouterProvider>(() => RouterProvider());
   sl.registerLazySingleton<ScoreCardProvider>(
-    () => ScoreCardProvider(const Uuid()),
+    () {
+      final config = Configuration.local([ScoreCard.schema]);
+      final realm = Realm(config);
+      return ScoreCardProvider(const u.Uuid(), realm);
+    },
   );
 }
