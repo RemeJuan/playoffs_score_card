6f040497ea5c933c12bc4668814e46e006b09c95
Reme Le Hane <mail@remej.dev>
2022-06-19 10:42:22 +0200

    fix(scores): enhanced page controls to enable progromatic navigation

--- lib/app_landing.dart
+++ lib/app_landing.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:playoffs_score_tracker/router/app_router.dart';
+import 'package:playoffs_score_tracker/router/router_provider.dart';
+import 'package:provider/provider.dart';
+
+import 'locator.dart';
 
 class AppLanding extends HookWidget {
   AppLanding({Key? key}) : super(key: key);
@@ -13,43 +17,50 @@ class AppLanding extends HookWidget {
 
   @override
   Widget build(context) {
-    final page = useState<int>(0);
+    return ChangeNotifierProvider<RouterProvider>.value(
+      value: sl<RouterProvider>(),
+      child: Consumer<RouterProvider>(
+        builder: (context, provider, _) {
+          final page = provider.currentIndex;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          AppRouter.routes[page.value].title,
-          style: TextStyle(
-            color: HexColor("#fb053e"),
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: HexColor("0c0033"),
-      ),
-      resizeToAvoidBottomInset: true,
-      body: PageView(
-        controller: pageController,
-        children: AppRouter.routes.map((route) {
-          return route.page;
-        }).toList(),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: page.value,
-        onTap: (index) {
-          pageController.animateToPage(
-            index,
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeInOut,
+          if (pageController.hasClients && pageController.page != page) {
+            pageController.animateToPage(
+              page,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
+          }
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                AppRouter.routes[page].title,
+                style: TextStyle(
+                  color: HexColor("#fb053e"),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              backgroundColor: HexColor("0c0033"),
+            ),
+            resizeToAvoidBottomInset: true,
+            body: PageView(
+              controller: pageController,
+              children: AppRouter.routes.map((route) {
+                return route.page;
+              }).toList(),
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: page,
+              onTap: provider.setCurrentIndex,
+              items: AppRouter.routes.map((route) {
+                return BottomNavigationBarItem(
+                  icon: Icon(route.icon),
+                  label: route.label,
+                );
+              }).toList(),
+            ),
           );
-
-          page.value = index;
         },
-        items: AppRouter.routes.map((route) {
-          return BottomNavigationBarItem(
-            icon: Icon(route.icon),
-            label: route.label,
-          );
-        }).toList(),
       ),
     );
   }
