323d62d62133d07fb625bfe47605473080e67f16
Reme Le Hane <mail@remej.dev>
2022-06-19 11:44:24 +0200

    fix(app): updated how real is registered on the locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:playoffs_score_tracker/router/router_provider.dart';
 import 'package:playoffs_score_tracker/schemas/score_card.schema.dart';
+import 'package:playoffs_score_tracker/views/history/provider/history.provider.dart';
 import 'package:playoffs_score_tracker/views/score_card/provider/score_card_provider.dart';
 import 'package:realm/realm.dart';
 import 'package:uuid/uuid.dart' as u;
@@ -8,12 +9,15 @@ import 'package:uuid/uuid.dart' as u;
 final sl = GetIt.instance;
 
 void initService() {
+  sl.registerLazySingleton<Realm>(() {
+    final config = Configuration.local([ScoreCard.schema], schemaVersion: 2);
+    return Realm(config);
+  });
   sl.registerLazySingleton<RouterProvider>(() => RouterProvider());
+  sl.registerLazySingleton<HistoryProvider>(
+    () => HistoryProvider(sl()),
+  );
   sl.registerLazySingleton<ScoreCardProvider>(
-    () {
-      final config = Configuration.local([ScoreCard.schema]);
-      final realm = Realm(config);
-      return ScoreCardProvider(const u.Uuid(), realm);
-    },
+    () => ScoreCardProvider(const u.Uuid(), sl()),
   );
 }
