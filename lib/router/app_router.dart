ec4df3679ffd7fb8b9a85c8ae6fab2a2c1e49fe3
Reme Le Hane <mail@remej.dev>
2022-06-19 07:12:32 +0200

    refactor(app): changed to custom router

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,13 +1,40 @@
-import 'package:auto_route/annotations.dart';
+import 'package:flutter/material.dart';
 import 'package:playoffs_score_tracker/views/history/history.view.dart';
 import 'package:playoffs_score_tracker/views/score_card/score_card.view.dart';
 
-@MaterialAutoRouter(
-  replaceInRouteName: 'View,Route,Screen',
-  preferRelativeImports: false,
-  routes: <AutoRoute>[
-    AutoRoute(page: ScoreCardView, initial: true),
-    AutoRoute(page: HistoryView),
-  ],
-)
-class $AppRouter {}
+class AppRoute {
+  final Widget page;
+  final String title;
+  final String label;
+  final IconData icon;
+
+  AppRoute({
+    required this.page,
+    required this.title,
+    required this.label,
+    required this.icon,
+  });
+}
+
+class AppRouter {
+  static final List<AppRoute> routes = [
+    AppRoute(
+      page: const ScoreCardView(),
+      title: "Score Card",
+      label: "Score",
+      icon: Icons.scoreboard_outlined,
+    ),
+    AppRoute(
+      page: const HistoryView(),
+      title: "History",
+      label: "History",
+      icon: Icons.history,
+    ),
+    AppRoute(
+      page: const SizedBox.shrink(),
+      title: "Profile",
+      label: "Profile",
+      icon: Icons.person_outline,
+    ),
+  ];
+}
